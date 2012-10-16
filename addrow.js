24bb38257e8b1567f39cf3b4abde8cc8516bfbbe
andrey.babkin <babkinandreyhm@gmail.com>
2012-10-16 16:21:42 +0400

    add work

--- addrow.js
+++ addrow.js
@@ -11,14 +11,17 @@ $(document).ready(
         var add_row_function=function(){
             var ds=EJSdata[$(this).attr('dataSource')];
             new EJS({url:$(this).attr('templateFile')}).update($(this).attr('targetElement'),ds);
-            ds._new[ds._new.length]=ds._newTemplate;
+            ds._new[ds._new.length]=jQuery.extend(true, {},ds._newTemplate);
         };
         $("input.add_row").each(add_row_function);
-
-
         $("input.add_row").live("click",add_row_function);
+        $('input').live("change",function() {
+            var data=EJSdata.rows._old;
+            if($(this).attr("variable")=="new"){
+                data=EJSdata.rows._new;
+            }
+            data[$(this).attr("key")][$(this).attr("property")]=$(this).val();
+            var a=2+2;
+        });
     }
-
-
-
 )
